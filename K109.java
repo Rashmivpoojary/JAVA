093367f2c35b57fe8e4fd9d60c0d5c6da399a9d3
Rashmivpoojary <rashmi.23cs116@sode-edu.in>
2024-10-24 10:56:48 +0530

    Add files via upload

---
+++ K109.java
@@ -0,0 +1,18 @@
+class MultiThreadOne extends Thread{
+ public void run(){
+   try{
+    System.out.println("Thread "+Thread.currentThread().getId()+"  is running");
+}
+  catch (Exception e){
+  System.out.println("Exception is caught");
+}
+}
+}
+public class K109{
+ public static void main(String[] args){
+   for(int i=0;i<8;i++){
+   MultiThreadOne mt1=new MultiThreadOne();
+   mt1.start();
+}
+}
+}
