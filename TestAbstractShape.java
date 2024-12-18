e20b38ab18dc63e09f67345fc81950d8f652a7d2
Rashmivpoojary <rashmi.23cs116@sode-edu.in>
2024-12-18 20:37:07 +0530

    Update and rename AbstractShape.java to TestAbstractShape.java

--- TestAbstractShape.java
+++ TestAbstractShape.java
@@ -26,7 +26,7 @@ class Triangle1 extends Shape1{
 	double calculatePerimeter() {
 		return side1+side2+side3;}}
 
-public class AbstractShape {
+public class TestAbstractShape {
 	public static void main(String[] args) {
 		Circle1 circle1=new Circle1(5);
 		System.out.println("Area and Circumference of Circle are");
