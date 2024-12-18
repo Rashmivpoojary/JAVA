d64cc140212958da2bfa5919486ddcb99dcc0435
Rashmivpoojary <rashmi.23cs116@sode-edu.in>
2024-12-18 20:35:02 +0530

    Update and rename Myshape.java to TestMyshape.java

--- TestMyshape.java
+++ TestMyshape.java
@@ -22,7 +22,7 @@ class Square extends Shape{
 	public void erase() {
 		System.out.println("Erasing a square");}}
 
-public class Myshape {
+public class TestMyshape {
 	public static void main(String[] args) {
 		Shape[] shapes=new Shape[3]; //array containing Shape
 		shapes[0]=new Circle();
